71bf9317044fbce0056ee9c8eeb0b1e8d5ecf282
ganeshbeldar015 <ganeshbeldar015@gmail.com>
2025-07-09 00:08:08 +0530

    change in Day2

---
+++ 04_Data_types/dataTypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    // 🔹 Basic Data Types
+    int age = 20;                 // Integer
+    float height = 5.9f;          // Float (f suffix denotes float)
+    double pi = 3.14159;          // Double
+    char grade = 'A';             // Character
+    bool passed = true;           // Boolean
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Pi: " << pi << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Passed: " << passed << endl;
+    return 0;
+
+}
