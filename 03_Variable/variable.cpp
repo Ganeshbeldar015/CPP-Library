e8862f063f5bee55e6fb5ffb47d122cefd0f503e
ganeshbeldar015 <ganeshbeldar015@gmail.com>
2025-07-08 23:49:23 +0530

    Day2

---
+++ 03_Variable/variable.cpp
@@ -0,0 +1,10 @@
+// containers to store a data 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 25;//age is a variable that can be  a store a value in memory
+    cout<<age;
+    return 0;
+}
