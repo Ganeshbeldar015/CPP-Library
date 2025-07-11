ec7c7cecce805440f08de5606ef3258e29faa778
ganeshbeldar015 <ganeshbeldar015@gmail.com>
2025-07-11 23:11:19 +0530

    changes

---
+++ PattenPrinting/squarePattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n=4;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+}
+
+/*Output 
+1234
+1234
+1234
+1234    
+*/
+
+
